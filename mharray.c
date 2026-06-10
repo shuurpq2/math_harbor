190dbdab1596de35531d48b318a380545faa3d9a
savethebees <afghanistaneurobolivar@gmail.com>
2026-06-10 17:25:59 +0300

    add some functions

--- mharray.c
+++ mharray.c
@@ -1,2 +1,74 @@
 #include "mharray.h"
 #include "thread_pull.h"
+#include <stdlib.h>
+#include <string.h>
+
+void free_mharray(MHArray mharray) {
+    if (mharray.data != NULL) {
+        free(mharray.data);
+    }
+
+    if (mharray.strides != NULL) {
+        free(mharray.strides);
+    }
+}
+
+void copy_mharray(MHArray *dest, const MHArray src) {
+    dest->data = malloc(src.size * sizeof(double));
+    for (int i = 0; i < src.size; i++) {
+        dest->data[i] = src.data[i];
+    }
+
+    dest->size = src.size;
+    dest->ndim = src.ndim;
+    
+    size_t strides_size = src.ndim * sizeof(int);
+    dest->strides = malloc(strides_size);
+    for (int i = 0; i < src.ndim; i++) {
+        dest->strides[i] = src.strides[i];
+    }
+}
+
+MHArray mharray_add_num(const MHArray mharray, const double num) {
+    MHArray result;
+    copy_mharray(&result, mharray);
+
+    for (int i = 0; i < mharray.size; i++) {
+        result.data[i] = mharray.data[i] + num;
+    }
+
+    return result;
+}
+
+MHArray mharray_substract_num(const MHArray mharray, const double num) {
+    MHArray result;
+    copy_mharray(&result, mharray);
+
+    for (int i = 0; i < mharray.size; i++) {
+        result.data[i] = mharray.data[i] - num;
+    }
+
+    return result;
+}
+
+MHArray mharray_mult_by_num(const MHArray mharray, const double num) {
+    MHArray result;
+    copy_mharray(&result, mharray);
+
+    for (int i = 0; i < mharray.size; i++) {
+        result.data[i] = mharray.data[i] * num;
+    }
+
+    return result;
+}
+
+MHArray mharrays_div_by_num(const MHArray mharray, const double num) {
+    MHArray result;
+    copy_mharray(&result, mharray);
+
+    for (int i = 0; i < mharray.size; i++) {
+        result.data[i] = mharray.data[i] / num;
+    }
+
+    return result;
+}
--- mharray.h
+++ mharray.h
@@ -3,8 +3,21 @@
 
 typedef struct {
     double* data;
-    int _ndim;
-    int* _strides;
+    int size;
+    int ndim;
+    int* strides;
 } MHArray;
+
+void free_mharray(MHArray mharray);
+
+void copy_mharray(MHArray *dest, const MHArray src);
+
+MHArray mharray_add_num(const MHArray mharray, const double num);
+
+MHArray mharray_substract_num(const MHArray mharray, const double num);
+
+MHArray mharray_mult_by_num(const MHArray mharray, const double num);
+
+MHArray mharrays_div_by_num(const MHArray mharray, const double num);
 
 #endif
